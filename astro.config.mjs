6e79e4d33e36c91504940bcb32a7094cc8333b41
Cloé Cordeiro <73696923+CloeCrdr@users.noreply.github.com>
2025-06-06 00:32:27 +0200

    Connect Vercel & correct env variables in project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import dotenv from "dotenv";
+import vercel from '@astrojs/vercel/serverless'; 
 
 dotenv.config()
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
+    adapter: vercel(),
     server: {
         host: true,
         port: 3000,
-    }
+    },
 });
